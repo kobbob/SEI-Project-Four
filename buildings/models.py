cd579c4cc0523cf49234ef2433ab3f1a2477c430
kobbok <kate.oboyle@icloud.com>
2022-06-15 09:42:28 +0100

    changed syntax in buildings model and now functioning correctly

--- buildings/models.py
+++ buildings/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Building(models.Model):
-    building_name: models.CharField(max_length=100, default=None)
-    building_location: models.CharField(max_length=100, default=None)
-    date_built: models.PositiveIntegerField(default=None)
-    architect: models.CharField(max_length=100, default=None)
-    description: models.CharField(max_length=500, default=None)
-    image_caption: models.CharField(max_length=50, default=None)
-    image_source: models.CharField(max_length=50, default=None)
-    image_url: models.URLField(default=None)
-    source_info: models.CharField(max_length=100, default=None)
-    source_url: models.URLField(default=None)
+    building_name = models.CharField(max_length=100, default=None)
+    building_location = models.CharField(max_length=100, default=None)
+    date_built = models.PositiveIntegerField(default=None)
+    architect = models.CharField(max_length=100, default=None)
+    description = models.CharField(max_length=500, default=None)
+    image_caption = models.CharField(max_length=50, default=None)
+    image_source = models.CharField(max_length=50, default=None)
+    image_url = models.URLField(default=None)
+    source_info = models.CharField(max_length=100, default=None)
+    source_url = models.URLField(default=None)
 
     def __str__(self):
       return f"{self.building_name} - {self.building_location}"
